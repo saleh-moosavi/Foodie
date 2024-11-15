5acd95bba7b83b80c4f825a248e079d6afd64cd3
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-11-15 16:45:52 +0330

    fix:Banner One mobile responsiveness

--- src/index/components/BannerOne.tsx
+++ src/index/components/BannerOne.tsx
@@ -1,16 +1,16 @@
 export default function BannerOne() {
   return (
-    <div className="grid grid-cols-2 xl:max-w-7xl xl:mx-auto my-20 overflow-hidden bg-gradient-to-l from-sky-100 rounded-[2.5rem]">
+    <div className="grid md:grid-cols-2 xl:max-w-7xl xl:mx-auto my-20 overflow-hidden bg-gradient-to-t md:bg-gradient-to-l from-sky-100 rounded-[2.5rem] mx-5">
       <article className="p-10 flex flex-col justify-center">
-        <h2 className="text-4xl font-bold leading-snug my-5">
+        <h2 className="text-xl md:text-4xl font-bold leading-snug my-5">
           Spicy Delicious Chicken Wings With More
         </h2>
-        <p className="text-gray-500 text-justify font-semibold">
+        <p className="text-gray-500 text-justify text-sm md:text-base font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
           error a totam, magni impedit soluta tempora temporibus tempore
           molestias.
         </p>
-        <button className="px-8 py-2 font-semibold bg-black text-white rounded-lg mt-10 w-fit">
+        <button className="px-8 py-2 text-sm md:text-base font-semibold bg-black text-white rounded-lg mt-10 w-fit">
           Learn More
         </button>
       </article>
