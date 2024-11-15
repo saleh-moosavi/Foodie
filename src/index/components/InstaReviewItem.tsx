b09ab98d8bc813d9a0d06d1b3416242f74dfe7c3
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-11-15 16:53:48 +0330

    fix:Insta Reviews mobile responsiveness

--- src/index/components/InstaReviewItem.tsx
+++ src/index/components/InstaReviewItem.tsx
@@ -6,7 +6,7 @@ import { GoKebabHorizontal } from "react-icons/go";
 
 export default function InstaReviewItem() {
   return (
-    <div className="bg-white overflow-hidden">
+    <div className="bg-white overflow-hidden w-72 lg:w-52 xl:w-60 mx-auto">
       <section className="flex justify-between px-2 items-center py-1">
         <div className="flex gap-x-5 items-center">
           <img
@@ -22,7 +22,7 @@ export default function InstaReviewItem() {
       </section>
       <section className="relative">
         <img
-          className="h-60 object-cover"
+          className="h-60 object-cover w-full"
           src="https://www.thespruceeats.com/thmb/CWfeSCngoL5zOxlR2tsBWETtZ3U=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/traditional-chicken-wings-912937-hero-01-6c1a003373a54538a732abc0005145d8.jpg"
         />
         <GoKebabHorizontal className="absolute bottom-0 right-[50%] translate-x-1/2 text-gray-100 w-8 h-8" />
--- src/index/components/InstaReviews.tsx
+++ src/index/components/InstaReviews.tsx
@@ -2,7 +2,7 @@ import InstaReviewItem from "./InstaReviewItem";
 
 export default function InstaReviews() {
   return (
-    <div className="flex flex-col items-center xl:max-w-7xl xl:mx-auto bg-gradient-to-t from-sky-100 p-20">
+    <div className="flex flex-col items-center xl:max-w-7xl xl:mx-auto bg-gradient-to-t from-sky-100 rounded-[2.5rem] p-5 md:p-20 mx-5">
       <article className="max-w-[35rem] text-center">
         <h2 className="text-4xl font-bold">Simple And Tasty Recipes</h2>
         <p className="text-gray-500 font-semibold mt-5">
@@ -10,7 +10,7 @@ export default function InstaReviews() {
           eius optio minima quia maxime aliquid in veniam, cum vitae.
         </p>
       </article>
-      <article className="grid grid-cols-4 gap-10 mt-10 w-full">
+      <article className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 w-full">
         <InstaReviewItem />
         <InstaReviewItem />
         <InstaReviewItem />
